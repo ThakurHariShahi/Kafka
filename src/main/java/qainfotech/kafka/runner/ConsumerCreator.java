4a58325937ed88b691ad2bbcf60827be6cb90f9e
Shahi Hari <harishahi@QAIT.COM>
2019-09-10 11:51:59 +0530

    https://dzone.com/articles/running-apache-kafka-on-windows-os

---
+++ src/main/java/qainfotech/kafka/runner/ConsumerCreator.java
@@ -0,0 +1,25 @@
+package qainfotech.kafka.runner;
+
+import java.util.Collections;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class ConsumerCreator {
+    public static Consumer<Long, String> createConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConstants.KAFKA_BROKERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConstants.GROUP_ID_CONFIG);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, kafkaConstants.MAX_POLL_RECORDS);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaConstants.OFFSET_RESET_EARLIER);
+        Consumer<Long, String> consumer = new KafkaConsumer<Long, String>(props);
+        consumer.subscribe(Collections.singletonList(kafkaConstants.TOPIC_NAME));
+        return consumer;
+    }
+}
